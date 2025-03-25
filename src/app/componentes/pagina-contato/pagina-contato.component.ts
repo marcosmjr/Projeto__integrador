3935af1775804539690a3e6af1627e16e00decbe
marcosmjr <marcos.moreira.jr@gmail.com>
2025-03-25 16:31:50 -0300

    20250325 Atualização da pagina de contatos

--- src/app/componentes/pagina-contato/pagina-contato.component.ts
+++ src/app/componentes/pagina-contato/pagina-contato.component.ts
@@ -6,6 +6,13 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 
 import {FormsModule} from '@angular/forms';
 import {MatCheckboxModule} from '@angular/material/checkbox';
+import { TextareaModule } from 'primeng/textarea';
+import {MatButtonModule} from '@angular/material/button';
+
+interface Estados {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-pagina-contato',
@@ -14,7 +21,9 @@ import {MatCheckboxModule} from '@angular/material/checkbox';
     MatInputModule, 
     MatSelectModule, 
     FormsModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    TextareaModule,
+    MatButtonModule
   ],
   templateUrl: './pagina-contato.component.html',
   styleUrl: './pagina-contato.component.css',
@@ -22,6 +31,37 @@ import {MatCheckboxModule} from '@angular/material/checkbox';
 })
 export class PaginaContatoComponent {
 
+  value: string = '';
+  estados: Estados[] = [
+    {value: 'SP', viewValue: 'SP'},
+    {value: 'AC', viewValue: 'AC'},
+    {value: 'AL', viewValue: 'AL'},
+    {value: 'AP', viewValue: 'AP'},
+    {value: 'AM', viewValue: 'AM'},
+    {value: 'BA', viewValue: 'BA'},
+    {value: 'CE', viewValue: 'CE'},
+    {value: 'DF', viewValue: 'DF'},
+    {value: 'ES', viewValue: 'ES'},
+    {value: 'GO', viewValue: 'GO'},
+    {value: 'MA', viewValue: 'MA'},
+    {value: 'MT', viewValue: 'MT'},
+    {value: 'MS', viewValue: 'MS'},
+    {value: 'MG', viewValue: 'MG'},
+    {value: 'PA', viewValue: 'PA'},
+    {value: 'PB', viewValue: 'PB'},
+    {value: 'PR', viewValue: 'PR'},
+    {value: 'PE', viewValue: 'PE'},
+    {value: 'PI', viewValue: 'PI'},
+    {value: 'RJ', viewValue: 'RJ'},
+    {value: 'RN', viewValue: 'RN'},
+    {value: 'RS', viewValue: 'RS'},
+    {value: 'RO', viewValue: 'RO'},
+    {value: 'RR', viewValue: 'RR'},
+    {value: 'SC', viewValue: 'SC'},
+    {value: 'SE', viewValue: 'SE'},
+    {value: 'TO', viewValue: 'TO'},
   
+  ];
+
 
 }
