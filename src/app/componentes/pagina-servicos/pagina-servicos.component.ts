dd7de12deb460940c7ba19d617441faf9148d7e6
marcosmjr <marcos.moreira.jr@gmail.com>
2025-04-11 23:46:25 -0300

    20250411 - Atualização do menu e criação da pagina serviços.

--- src/app/componentes/pagina-servicos/pagina-servicos.component.ts
+++ src/app/componentes/pagina-servicos/pagina-servicos.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {ChangeDetectionStrategy} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 
 @Component({
   selector: 'app-pagina-servicos',
-  imports: [],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './pagina-servicos.component.html',
   styleUrl: './pagina-servicos.component.css'
 })
